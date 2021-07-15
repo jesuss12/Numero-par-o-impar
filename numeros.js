714bc6e475bc91755d8f3c7a19b1d1c80d92ab86
jesuss12 <87486974+jesuss12@users.noreply.github.com>
2021-07-15 11:47:41 -0500

    Create numeros.js

---
+++ numeros.js
@@ -0,0 +1,25 @@
+/**
+ *
+ * cualquier duda sobre este codigo, shamirdhc31@gmail.com
+ */
+import java.util.Scanner;
+public class EJERCICIO_02
+{
+    public static void main(String[] ARGS)
+    {
+        Scanner LEER = new Scanner(System.in);
+        int NUMERO;
+
+        System.out.print("INGRESE EL NUMERO : ");
+        NUMERO = LEER.nextInt();
+
+        if(NUMERO % 2 == 0 )
+        {
+            System.out.println("EL NUMERO QUE USTED INGRESO ES PAR");
+        }
+        else
+        {
+            System.out.println("EL NUMERO QUE USTED INGRESO ES IMPAR");
+        }
+    }
+}
